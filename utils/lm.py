4907fc7c944b064c5b62d6d3cfb2fc8d239f80fe
ChangyWen <ddcwen@gmail.com>
2025-07-03 20:12:01 +0800

    feat: precise-wiki data generation

--- utils/lm.py
+++ utils/lm.py
@@ -50,26 +50,30 @@ model_map = {   'meta-llama/Llama-3.1-405B-Instruct-FP8': {'name': 'llama3.1_405
 ########################################################################################################
 
 def call_vllm_api(prompt, model, temperature=0.0, top_p=1.0, max_tokens=512, port=None, i=0):
-    # if port == None:
-    #     port = model_map[model]["server_urls"][i]
+    try:
+        # if port == None:
+        #     port = model_map[model]["server_urls"][i]
 
-    client = openai.OpenAI(
-        base_url=f"http://localhost:8000/v1",
-        api_key="NOT A REAL KEY",
-    )
-    chat_completion = client.chat.completions.create(
-        model=model,
-        messages=[{"role": "user","content": prompt}],
-        max_tokens=max_tokens,
-        temperature=0.0,
-        top_p=1.0
-    )
+        client = openai.OpenAI(
+            base_url=f"http://localhost:8000/v1",
+            api_key="NOT A REAL KEY",
+        )
+        chat_completion = client.chat.completions.create(
+            model=model,
+            messages=[{"role": "user","content": prompt}],
+            max_tokens=max_tokens,
+            temperature=0.0,
+            top_p=1.0
+        )
 
-    print(f"calling call_vllm_api with model {model}")
-    print(f"prompt: {prompt}")
-    print(f"response: {chat_completion.choices[0].message.content}")
+        print(f"calling call_vllm_api with model {model}")
+        print(f"prompt: {prompt}")
+        print(f"response: {chat_completion.choices[0].message.content}")
 
-    return chat_completion.choices[0].message.content
+        return chat_completion.choices[0].message.content
+    except Exception as e:
+        print(e)
+        return ""
 
 def openai_generate(prompt, model, temperature=0.0, top_p=1.0, max_tokens=512):
     try:
